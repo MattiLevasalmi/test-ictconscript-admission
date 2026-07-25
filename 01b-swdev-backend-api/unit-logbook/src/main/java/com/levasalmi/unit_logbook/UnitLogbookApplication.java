251a8f0ead3bfc93f7eda6b321380dd3a6cdf647
Matti Levasalmi <levasalmi.matti@gmail.com>
2026-07-25 10:10:26 +0000

    Create base springboot project with dockerfile

---
+++ 01b-swdev-backend-api/unit-logbook/src/main/java/com/levasalmi/unit_logbook/UnitLogbookApplication.java
@@ -0,0 +1,13 @@
+package com.levasalmi.unit_logbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitLogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitLogbookApplication.class, args);
+	}
+
+}
---
+++ 01b-swdev-backend-api/unit-logbook/src/test/java/com/levasalmi/unit_logbook/UnitLogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levasalmi.unit_logbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitLogbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
